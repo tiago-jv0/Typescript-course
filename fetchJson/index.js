83831db548c5edf2ff0375a9687dc6d1bbdd4774
Tiago José <tiago_jvo@outlook.com>
2020-06-27 00:30:48 -0300

    Some examples of type annotations

---
+++ fetchJson/index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
+var url = 'https://jsonplaceholder.typicode.com/todos';
+axios_1["default"].get(url).then(function (data) { return console.log(data); });
